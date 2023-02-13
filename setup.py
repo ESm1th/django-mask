dcc4be058cf9cc37e1eff51f24d32c28f180afa0
evgeniy_kuznetsov <e.kuznetsov@agora.ru>
2023-02-13 23:38:47 +0300

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,3 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='django_mask',
+    version="0.1",
+    license='BSD',
+    description='Model fields masking.',
+    author='Evgeny Kuznetsov',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=["django<=3.2.13"],
+    python_requires="<=3.10.10",
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
